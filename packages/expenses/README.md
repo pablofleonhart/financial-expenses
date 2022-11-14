a2792441c00d113328cfe293b6350761aaf84b0e
Pablo Felipe Leonhart <pablo.fl11@gmail.com>
2022-11-14 22:26:04 +0100

    Creating structure for the application

--- packages/expenses/README.md
+++ packages/expenses/README.md
@@ -1,4 +1,4 @@
-# financial-app
+# expenses
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ packages/shell/vite.config.ts
@@ -0,0 +1,14 @@
+import federation from "@originjs/vite-plugin-federation";
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
