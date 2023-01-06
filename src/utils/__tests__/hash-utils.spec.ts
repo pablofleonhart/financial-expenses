f0ad4b71af48216cd64334ccb38dd3fd58fd3563
Pablo Felipe Leonhart <pablo.fl11@gmail.com>
2023-01-06 18:31:23 +0100

    Include hash tests

---
+++ src/utils/__tests__/hash-utils.spec.ts
@@ -0,0 +1,20 @@
+import { describe, expect, it } from 'vitest';
+import { compare, decrypt, encrypt } from '../hash-utils';
+
+describe('Hash utils tests', () => {
+  it('should encrypt a string', () => {
+    expect(encrypt('ABC123')).toString()
+  });
+
+  it('should decrypt a string', () => {
+    const encryptedValue = encrypt('ABC123')
+    expect(encryptedValue).toString()
+    expect(decrypt(encryptedValue)).toString();
+  });
+
+  it('should encrypt and decrypt match the string', () => {
+    const encryptedValue = encrypt('ABC123')
+    expect(encryptedValue).toString()
+    expect(compare('ABC123', encryptedValue)).toBeTruthy()
+  });
+});
