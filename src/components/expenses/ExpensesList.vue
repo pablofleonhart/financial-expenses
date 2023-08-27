7b4bd6012e74ff4ae435533f4d243f8bb6b915c3
Pablo Felipe Leonhart <pablo.fl11@gmail.com>
2023-08-27 19:35:23 +0200

    cleanning branch

--- src/components/expenses/ExpensesList.vue
+++ src/components/expenses/ExpensesList.vue
@@ -90,7 +90,7 @@
                   class="button-action text-neutral-color-off hover:bg-red-500 hover:text-neutral-50"
                   @click="onDeleteExpense(index)"
                 />
-                <ph-thumbs-up
+                <!-- <ph-thumbs-up
                   class="button-action hover:text-green-500"
                   :class="{
                     'text-green-500': item.fullfilled,
@@ -98,7 +98,7 @@
                   }"
                   weight="fill"
                   @click="handleRevenue(index)"
-                />
+                /> -->
               </div>
             </td>
           </tr>
@@ -188,12 +188,12 @@ const onDeleteExpense = (index: number) => {
   emit('onDeleteExpense', expensesList.value[index]);
 };
 
-const handleRevenue = async (index: number) => {
-  const item: Expense = expensesList.value[index];
-  if (item.fullfilled) {
-    // await reopenRevenue(expensesList.value[index]);
-  } else {
-    // await fulfillRevenue(expensesList.value[index]);
-  }
-};
+// const handleRevenue = async (index: number) => {
+//   const item: Expense = expensesList.value[index];
+//   if (item.fullfilled) {
+//     // await reopenRevenue(expensesList.value[index]);
+//   } else {
+//     // await fulfillRevenue(expensesList.value[index]);
+//   }
+// };
 </script>
